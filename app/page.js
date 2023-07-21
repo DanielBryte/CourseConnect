003f680f7203ad659624c63c11e51a3b7df061ee
Daniel Bryte <danielbryte1@gmail.com>
2023-07-21 20:16:17 +0100

    feat: update hero text and description

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ import Feed from '@components/Feed'
 const Home = () => {
   return (
       <section className="w-full flex-center flex-col">
-         <h1 className="head_text text-center">Discover and Learn
+         <h1 className="head_text text-center">Share and Discover
           <br className="max-md:hidden"/>
-          <span className="text-[#131972] text-center"> Top tech courses</span>
+          <span className="text-[#131972] text-center"> Top Tech Courses</span>
          </h1>
          <p className="desc text-center">
-          Course Connect is an open source course aggregator from various industry experts and edtech platforms built for techies to aid them discover, learn, grow, and build on high quality courses and resources.
+          Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.
          </p>
-         {/* {Feed} */}
+         <Feed/>
       </section>
   )
 }
