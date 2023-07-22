a1932216a956980cf2fc6b3ce7ea1e859439eaf7
Daniel Bryte <danielbryte1@gmail.com>
2023-07-22 12:14:45 +0100

    feat: implement site footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import Image from "next/image"
+
 
 const Footer = () => {
   return (
-    <div className='text-3xl text-black'>Footer</div>
+    <div className='footer'>
+
+      <div className='flex items-center font-bold text-3xl gap-2'>
+        <Image
+          className='object-obtain'
+          src="/assets/images/logo.svg"
+          width={35}
+          height={35}
+          alt='logo'
+        />
+        <p className='footer-text'>Course Connect</p>
+      </div>
+
+      <p className="w-full lg:w-3/6 text-center">Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.</p>
+
+
+      <span className="text-md -mb-6">An open source product from <a target="_blank" className="text-blue-400" href="https://www.acabeta.com"> Acabeta</a> maintained by <a className="text-blue-400" target="_blank" href="https://www.github.com/danielbryte"> Daniel Bryte.</a></span>
+    </div>
   )
 }
 
