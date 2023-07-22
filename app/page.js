ae50ce08038c3966ac2570b1923a40009e20f8a7
Daniel Bryte <danielbryte1@gmail.com>
2023-07-22 10:10:22 +0100

    style: fix hero text to theme color

--- app/page.js
+++ app/page.js
@@ -3,16 +3,16 @@ import Feed from '@components/Feed'
 
 const Home = () => {
   return (
-      <section className="w-full flex-center flex-col">
-         <h1 className="head_text text-center">Share and Discover
-          <br className="max-md:hidden"/>
-          <span className="text-[#131972] text-center"> Top Tech Courses</span>
-         </h1>
-         <p className="desc text-center">
-          Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.
-         </p>
-         <Feed/>
-      </section>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Share and Discover
+        <br className="max-md:hidden"/>
+        <span className="text-primary text-center"> Top Tech Courses</span>
+      </h1>
+      <p className="desc text-center">
+        Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.
+      </p>
+      <Feed/>
+    </section>
   )
 }
 
