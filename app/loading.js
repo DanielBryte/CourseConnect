266a804b028aae0f962ec09a180b901122431fa5
Daniel Bryte <danielbryte1@gmail.com>
2023-07-24 00:47:40 +0100

    style: fix loader ui

--- app/loading.js
+++ app/loading.js
@@ -1,6 +1,5 @@
 export default function Loading() {
   return (
-    <div className="my-5">
-      <p className="text-[3rem] text-center text-primary p-8 animate-spin border rounded-full">o</p>
-    </div>)
+      <p className="text-[5rem] text-center text-primary p-8 animate-spin border rounded-full">o</p>
+)
 }
