1258159ae5aba97d36c6b8e97125ccf5b9708b94
Daniel Bryte <danielbryte1@gmail.com>
2023-07-23 03:22:35 +0100

    feat: implement loading.js while fetching

--- app/layout.js
+++ app/layout.js
@@ -2,31 +2,34 @@ import '@styles/globals.css'
 import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 import Footer from '@components/Footer'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 
 export const metadata = {
     title: "Course Connect",
     description: "Find useful code snippets",
 }
-const RootLayout = ({children}) => {
-  return (
-    <html lang='en'>
-        <body>
-            <Provider>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
 
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-            </Provider>
-
-            <Footer/>
-        </body>
-    </html>
-  )
+                    <main className='app'>
+                        <Nav/>
+                        <Suspense fallback={<Loading />}>
+                            {children}
+                        </Suspense>
+                    </main>
+                </Provider>
+                <Footer />
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
