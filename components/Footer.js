f5bdbf2e9581f0f2297291a5061fba59d7d194ce
Daniel Bryte <danielbryte1@gmail.com>
2023-07-24 16:20:32 +0100

    style: fix footer alignment

--- components/Footer.js
+++ components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
         <p className='footer-text'>Course Connect</p>
       </div>
 
-      <p className="w-full lg:w-3/6 text-center">Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.</p>
+      <p className="w-full lg:w-3/6">Course Connect is an open source course & resource aggregator from various industry experts and edtech platforms. Built for techies to aid them discover, learn, grow, and build on high quality courses and resources.</p>
 
 
       <span className="text-md -mb-6">An open source product from <a target="_blank" className="text-blue-400" href="https://www.acabeta.com"> Acabeta</a> maintained by <a className="text-blue-400" target="_blank" href="https://www.github.com/danielbryte"> Daniel Bryte.</a></span>
