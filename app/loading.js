8ddeec9651c0885612035b1ba08fbd5131b7753e
Daniel Bryte <danielbryte1@gmail.com>
2023-08-01 12:35:18 +0100

    fix: loading js

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,5 @@
 export default function Loading() {
   return (
-      <p className="text-[5rem] text-center text-primary p-8 animate-spin border rounded-full">o</p>
+      <p className="relative h-full w-full text-[5rem] text-center text-primary p-8 animate-spin border rounded-full">o</p>
 )
 }
